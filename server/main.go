0f2c9b5d1adaa6e28a23655a1be2293e0f0e9c88
Gabryjiel <gabriel.kudyba@gmail.com>
2024-05-29 08:50:14 +0200

    Add server/main.go with basic templ template
    Nothing much so far, but it works.

--- server/main.go
+++ server/main.go
@@ -1,7 +1,78 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"database/sql"
+	"errors"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/Gabryjiel/solar-system/internal"
+	"github.com/Gabryjiel/solar-system/server/mytemplates"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+)
 
 func main() {
-	fmt.Println("Testing")
+	workingDir, err := os.Getwd()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = godotenv.Load(workingDir + "/.env")
+
+	if err != nil {
+		log.Fatal("Error loading .env file from", workingDir+"/.env")
+	}
+	log.Println("[Collector] Env file loaded")
+
+	db, err := sql.Open("postgres", os.Getenv("DB_URL"))
+
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+	log.Println("[Collector] Database connected")
+
+	staticHandler := http.StripPrefix(
+		"/dist",
+		http.FileServer(http.Dir("./templates/dist")),
+	)
+
+	mux := http.NewServeMux()
+	mux.Handle("GET /dist/", staticHandler)
+	mux.Handle("GET /", handlerHomepage(db))
+	err = http.ListenAndServe(":8888", mux)
+
+	if errors.Is(err, http.ErrServerClosed) {
+		log.Println("Server closed")
+	} else if err != nil {
+		log.Println("Error starting server: ", err)
+	}
+}
+
+func handlerHomepage(db *sql.DB) http.Handler {
+	return http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
+		var log internal.Row
+
+		row := db.QueryRow("SELECT * FROM logs ORDER BY timestamp DESC LIMIT 1;")
+		err := row.Scan(
+			&log.Id,
+			&log.Cover_sta_rssi,
+			&log.Timestamp,
+			&log.Energy_total,
+			&log.Energy_today,
+			&log.Power_now,
+			&log.Utime,
+			&log.Alarm,
+		)
+
+		if err != nil {
+			panic(err)
+		}
+
+		_ = mytemplates.Home(log).Render(context.Background(), res)	
+	})
 }
